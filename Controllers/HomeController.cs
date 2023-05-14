eca3bd3898dbb1c01c8724d4ec0bc3aaffa2c273
Abdullah A. Bahraq <abdallahbhrag@gmail.com>
2023-05-14 19:42:39 +0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Convert_To_MVC.Models;
+
+namespace Convert_To_MVC.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult About()
+    {
+        return View();
+    }
+    public IActionResult Blog()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
+    public IActionResult Services()
+    {
+        return View();
+    }
+    public IActionResult Single()
+    {
+        return View();
+    }
+
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Convert_To_MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
